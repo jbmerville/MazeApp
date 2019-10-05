3d8a0b6ce6ab1c6710e5bf97099154ff58651d86
jbmerville <jbmer@my.yorku.ca>
2019-10-05 14:37:36 -0400

    added start/end/visting types of cells

--- src/Cell.js
+++ src/Cell.js
@@ -1,21 +1,33 @@
 import React from "react";
 
 class Cell extends React.Component {
-
-
     constructor(props) {
         super(props);
         let cls = "default";
-        if(props.type === 0){
+        if (props.type === 0) {
             cls = "unvisited";
         }
-        this.state = {cls: cls};
+        if (props.type == 1) {
+            cls = "walls";
+        }
+        if (props.type == 2) {
+            cls = "start";
+        }
+        if (props.type == 3) {
+            cls = "end";
+        }
+        if (props.type == 4) {
+            cls = "visiting";
+        }
+        this.state = { cls: cls };
     }
 
     render() {
         return (
-        <div className={"cell " + this.state.cls} key={this.state.row + "-" + this.state.column}>
-        </div>
+            <div
+                className={"cell " + this.state.cls}
+                key={this.state.row + "-" + this.state.column}
+            ></div>
         );
     }
 }
