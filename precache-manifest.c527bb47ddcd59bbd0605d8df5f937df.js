802863a2df18c87f139356de5aa3f996fb729922
jbmerville <jbmer@my.yorku.ca>
2020-04-06 17:19:34 +0200

    Updates

---
+++ precache-manifest.c527bb47ddcd59bbd0605d8df5f937df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8876307617b2a6da9e404ccdca5b29df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94d68ee399509ed2ea2",
+    "url": "/static/js/2.7e03b025.chunk.js"
+  },
+  {
+    "revision": "c1622e814717ca020047",
+    "url": "/static/js/main.4e2e7911.chunk.js"
+  },
+  {
+    "revision": "85f1040416dd829d6cc1",
+    "url": "/static/js/runtime-main.549fde0e.js"
+  }
+]);
